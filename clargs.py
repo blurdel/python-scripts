cf92fc90fd68c053287dc4fdcb12adfb1b073f99
blurdel <5648737+blurdel@users.noreply.github.com>
2023-03-15 15:23:07 -0400

    Added optional arg with a default value

--- clargs.py
+++ clargs.py
@@ -1,17 +1,17 @@
 import argparse
 import os
-import parser
 
 
 # Command line params
-argMap = { "fname": "none.txt", "-v": False }
+argMap = { "fname": "none.txt", "-v": False, "count": 0 }
 
 def main() :
 
     print("cwd:", os.getcwd())
 
-    parser = argparse.ArgumentParser("arg parse")
+    parser = argparse.ArgumentParser(prog="clargs")
     parser.add_argument("-f", required=True, metavar="filename", help="The input filename")
+    parser.add_argument("-n", required=False, type=int, default=1, help="the count n to iterate")
     parser.add_argument("-v", action='store_true', help="Show version info and exit")
 
     try:
@@ -20,13 +20,15 @@ def main() :
 
         if not args.f is None:
             argMap['fname'] = args.f.strip()
+        
+        argMap['count'] = args.n
         argMap['-v'] = args.v
 
     except Exception as e:
         print("Exception:", str(e))
         return -1
 
-
+    print()
     print("### argMap contents ###")
     for key, val in argMap.items():
         print("{}: {}".format(key, val))
@@ -35,6 +37,8 @@ def main() :
     if not os.path.exists(argMap['fname']):
         print("Error: input file not found, exiting ...")
         return 1
+    else:
+        print("Input file was found!")
 
 
 if __name__ == "__main__":
